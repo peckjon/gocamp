f30946753f4f8e12d782cac307f1c251f7a50f78
Jon Peck <peckjon@gmail.com>
2019-06-03 11:52:10 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='GoCamp',
+      version='0.1',
+      description='A better way to find campsites',
+      author='Jon Peck',
+      author_email='peckjon@gmail.com',
+      url='https://github.com/peckjon/',
+      packages=find_packages(),
+     )
