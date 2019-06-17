034fce7da6d9d43e5500eb79466ef4fb453cfccd
Jon Peck <peckjon@gmail.com>
2019-06-17 10:43:50 -0700

    refactor for clarity get dates from user

---
+++ app.py
@@ -0,0 +1,127 @@
+from collections import OrderedDict
+
+from models import Camp, CampArea, Equipment, ResourceCategory, Site, SiteAvailability
+import requests
+
+ENDPOINTS = {
+    'LIST_RESOURCECATEGORY': 'https://washington.goingtocamp.com/api/resourcecategory',
+    'LIST_EQUIPMENT': 'https://washington.goingtocamp.com/api/equipment',
+    'LIST_RESOURCESTATUS': 'https://washington.goingtocamp.com/api/availability/resourcestatus',
+    'MAPDATA': 'https://washington.goingtocamp.com/api/maps/mapdatabyid',
+    'LIST_CAMPGROUNDS': 'https://washington.goingtocamp.com/api/resourcelocation/rootmaps',
+    'SITE_DETAILS': 'https://washington.goingtocamp.com/api/resource/details',
+    'CAMP_DETAILS': 'https://washington.goingtocamp.com/api/resourcelocation/locationdetails',
+    'DAILY_AVAILABILITY': 'https://washington.goingtocamp.com/api/availability/resourcedailyavailability',
+}
+
+
+def get_json(endpoint, params=None):
+    return requests.get(ENDPOINTS[endpoint], params=params).json()
+
+
+def post_json(endpoint, data=None):
+    return requests.post(ENDPOINTS[endpoint], headers={'Content-Type': 'application/json'}, json=data).json()
+
+
+def get_reservation_link(party_size, start_date, end_date, map_id, resource_location_id, equipment_id, sub_equipment_id):
+    return\
+        'https://washington.goingtocamp.com/create-booking/results?mapId=%s&bookingCategoryId=0&startDate=%s&endDate=%s&isReserving=true&equipmentId=%s&subEquipmentId=%s&partySize=%s&resourceLocationId=%s'\
+        %(map_id, start_date.isoformat(), end_date.isoformat(), equipment_id, sub_equipment_id, party_size, resource_location_id)
+
+
+def list_resource_categorys():
+    return [ResourceCategory(e['resourceCategoryId'],e['localizedValues'][0]['name']) for e in get_json('LIST_RESOURCECATEGORY')]
+
+
+def list_equipments():
+    equipments = []
+    equipment_all = sorted(get_json('LIST_EQUIPMENT'), key=lambda e: e['order'])
+    for category in equipment_all:
+        equipment = category['subEquipmentCategories']
+        equipments.extend([Equipment(category['equipmentCategoryId'],e['subEquipmentCategoryId'],e['localizedValues'][0]['name']) for e in equipment])
+    return equipments
+
+
+def list_camps(resource_category_id):
+    camps = []
+    for camp in get_json('LIST_CAMPGROUNDS'):
+        if camp['resourceLocationId'] and resource_category_id in camp['resourceCategoryIds']:
+            camp = Camp(camp['resourceLocationLocalizedValues']['en-US'], camp['mapId'], camp['resourceLocationId'])
+            camps.append(camp)
+    return camps
+
+
+def get_camp_detail(resourcelocationid):
+    return get_json('CAMP_DETAILS', {'resourceLocationId':resourcelocationid})
+
+
+def get_site_detail(resourceId):
+    return get_json('SITE_DETAILS', {'resourceId':resourceId})
+
+
+def list_camp_areas(mapid, start_date, end_date, equipment_type):
+    data = {
+       'mapId':mapid,
+       'bookingCategoryId':0,
+       'startDate':start_date.isoformat(),
+       'endDate':end_date.isoformat(),
+       'isReserving':True,
+       'getDailyAvailability':True,
+       'partySize':1,
+       'equipmentId':equipment_type,
+       'subEquipmentId':equipment_type,
+       'generateBreadcrumbs':False,
+    }
+    results = post_json('MAPDATA', data)
+    camp_areas = []
+    for map_id, info in results['mapLinkLocalizedValues'].items():
+        for entry in info:
+            camp_areas.append(CampArea(map_id, entry['title'],entry['description']))
+    return camp_areas
+
+
+def list_sites(map_id, start_date, end_date, equipment_type):
+    data = {
+       'mapId':map_id,
+       'bookingCategoryId':0,
+       'startDate':start_date,
+       'endDate':end_date,
+       'isReserving':True,
+       'getDailyAvailability':True,
+       'partySize':1,
+       'equipmentId':equipment_type,
+       'subEquipmentId':equipment_type,
+       'generateBreadcrumbs':False,
+    }
+    results = post_json('MAPDATA', data)
+    sites = []
+    for entry in results['resourcesOnMap']:
+        sites.append(Site(entry['resourceId'], entry['localizedValues'][0]['name'],entry['localizedValues'][0]['description']))
+    sites.sort(key=lambda site: site.name.zfill(3))
+    return sites
+
+
+def list_site_availability(map_id, start_date, end_date, equipment_type):
+    data = {
+       'mapId':map_id,
+       'bookingCategoryId':0,
+       'startDate':start_date.isoformat(),
+       'endDate':end_date.isoformat(),
+       'isReserving':True,
+       'getDailyAvailability':True,
+       'partySize':1,
+       'equipmentId':equipment_type,
+       'subEquipmentId':equipment_type,
+       'generateBreadcrumbs':False,
+    }
+    results = post_json('MAPDATA', data)
+    sites_availability = {}
+    for entry in results['resourcesOnMap']:
+        site = Site(entry['resourceId'], entry['localizedValues'][0]['name'],entry['localizedValues'][0]['description'])
+        allowed_equipment = [Equipment(e['item1'],e['item2'], None) for e in entry['allowedEquipment']]
+        availability = [
+            SiteAvailability(site, e['availability'], allowed_equipment)
+            for e in results['resourceAvailabilityMap'][str(site.resource_id)]
+        ]
+        sites_availability[site] = availability
+    return OrderedDict(sorted(sites_availability.items(), key=lambda sa: sa[0].name.zfill(3)))
---
+++ console.py
@@ -0,0 +1,79 @@
+import datetime
+
+from dateutil import parser
+
+from app import *
+
+
+def prompt_int(prompt, default=None, print_selection=False):
+    if default==None:
+        selection = int(input('%s: ' % prompt))
+    else:
+        selection = input('%s [%s]: ' % (prompt, default))
+        selection = default if len(selection.strip()) == 0 else int(selection)
+    if print_selection:
+        print(selection)
+    return selection
+
+
+def prompt_collection(prompt, collection, default=None, print_selection=False):
+    for i, e in enumerate(collection):
+        print('%i: %s' % (i, e))
+    if default==None:
+        selection = collection[int(input('%s: ' % prompt))]
+    else:
+        selection = input('%s [%s]: ' % (prompt, default))
+        selection = collection[default if len(selection.strip()) == 0 else int(selection)]
+    if print_selection:
+        print(selection)
+    return selection
+
+
+def print_site_availabilitys(site, dates, site_availabilitys):
+    print(site)  # get_site_detail(resourceId)?
+    for i, site_availability in enumerate(site_availabilitys):
+        site_availability_text = 'not available'
+        if site_availability.availability == 0:
+            site_allowed_equipment_text = [(e.category_id, e.subcategory_id) for e in
+                                           site_availability.allowed_equipment]
+            site_availability_text = 'AVAILABLE' if equipment_id_subid in site_allowed_equipment_text else 'equipment not allowed'
+        print('  %s %s' % (dates[i], site_availability_text))
+
+
+if __name__ == '__main__':
+
+    # how many people?
+    party_size = prompt_int('NUMBER OF PEOPLE', 2)
+
+    # camping dates
+    now = datetime.datetime.now(datetime.timezone.utc)
+    year = prompt_int('Year', now.year)
+    start_month = prompt_int('Start Month', now.month)
+    start_day = prompt_int('Start Day', now.day)
+    end_month = prompt_int('End Month', now.month)
+    end_day = prompt_int('End Day', now.day)
+    start_date = datetime.datetime(year, start_month, start_day, 7, tzinfo=datetime.timezone.utc)
+    end_date = datetime.datetime(year, end_month, end_day, 7, tzinfo=datetime.timezone.utc)
+    dates = [(start_date + datetime.timedelta(days=x)).strftime('%y-%b-%d') for x in range(0, (end_date+datetime.timedelta(days=2)-start_date).days)]
+
+    # campsite, cabin, etc... TBD: does this work for non-campsites?
+    resource_category = prompt_collection('SELECT A CATEGORY', list_resource_categorys(), None, True)
+
+    # pick tent, RV, etc
+    equipment = prompt_collection('SELECT EQUIPMENT', list_equipments(), None, True)
+
+    # pick a camp
+    camp = prompt_collection('SELECT A CAMP', list_camps(resource_category.resource_id))
+    detail = get_camp_detail(camp.resource_location_id)
+    print(detail['localizedDetails'][0]['description'])
+
+    # pick section of the camp
+    camp_area = prompt_collection('SELECT A CAMP AREA', list_camp_areas(camp.map_id, start_date, end_date, equipment.subcategory_id), None, True)
+
+    # list availability
+    equipment_id_subid = (equipment.category_id, equipment.subcategory_id)
+    for site, site_availabilitys in list_site_availability(camp_area.map_id, start_date, end_date, equipment.subcategory_id).items():
+        print_site_availabilitys(site, dates, site_availabilitys)
+
+    # web link to make reservation
+    print(get_reservation_link(party_size, start_date, end_date, camp_area.map_id, camp.resource_location_id, equipment.category_id, equipment.subcategory_id))
